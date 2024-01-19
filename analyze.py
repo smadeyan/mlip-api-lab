f7d058ab413e75e5aa627da341f55aa5d38dcf50
Shrey Madeyanda <smadeyan@andrew.cmu.edu>
2024-01-18 22:09:15 -0500

    Read key from environment variable

--- analyze.py
+++ analyze.py
@@ -2,9 +2,10 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = "https://aie-hw1.cognitiveservices.azure.com/"
+key = os.environ.get('AZURE_KEY', 'default')
 
 credentials = CognitiveServicesCredentials(key)
 
